3faa2edf6d6635b748b083819e01e4b5525de4ac
KL <KokLiangC@genstudents.org>
2026-05-29 11:29:08 +0800

    Rule5Fist Structural Alignment: Integrated HtmlWebpackPlugin to bind public/index.html with bundle delivery target

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 ﻿const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -18,10 +20,18 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html'
+    }),
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
